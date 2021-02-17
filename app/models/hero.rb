acb26c121f82ca5c5e8199248ed99d6f6d4f7f67
Kellen <kellenbmay@gmail.com>
2021-02-17 15:06:40 -0500

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,18 @@
+class Hero
+    attr_accessor :name, :power, :biography 
+
+    @@heros = []
+
+    def initialize(params)
+        @name = params[:name]
+        @power = params[:power]
+        @biography = params[:biography]
+        @@heros << self
+    end
+
+    def self.all
+        @@heros
+    end
+
+
+end
